5f2b2f513b0e9e65d97977a25017c79845216bc1
Patrick <ahrntal.patrick@gmail.com>
2019-10-22 11:20:57 +0200

    add basic auth to backend

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,31 @@ const { Client } = require('pg');
 var Pool = require('pg-pool');
 var dbconnection = require ('./dbconnection.js');
 
+var auth = require('basic-auth');
+
+var username = 'patrick'; //process.env.ENV_API_USER;
+var password = 'patrick'; //process.env.ENV_API_PASS;
+
 app.listen(3000)
 
 
-
-app.get('/api', function (req, res) {
-    res.send('MyFinance API is Running! :)');
+app.get('/', function (req, res) {
+  res.send('MyFinance API is Running! :)');
 })
+
+
+app.use('/api',function(req, res, callback) {
+  var user = auth(req);
+
+  if (user === undefined || user['name'] !== username || user['pass'] !== password) {
+      res.statusCode = 401;
+      res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+      res.end('Unauthorized');
+  } else {
+      callback();
+  }
+});
+
 
 app.get('/api/entries/:user', function (req, result) {
     
