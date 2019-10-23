ce7da372da0f16f00946a2d0a05c735c9e03e2dc
Patrick <ahrntal.patrick@gmail.com>
2019-10-23 20:38:52 +0200

    note column added to backend + note endpoint created

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.get('/api/entries/:user', function (req, result, callback) {
   
     pool.connect().then(client => {
       client.query(
-        "select value, crdt from MON_ENTRIES where crby = '" + user + "';"
+        "select value, to_char(crdt, 'DD.MM.YYY  HH12:MI:SS') as crdt, note from MON_ENTRIES where crby = '" + user + "' order by crdt;"
       ).then(res => {
         if(res && res.rowCount != 0){
           result.send(res.rows);
@@ -60,6 +60,39 @@ app.get('/api/entries/:user', function (req, result, callback) {
         return null;
       })
       })
+});
+
+app.get('/api/new/:user/:value/:note', function(req, result, callback){
+
+  try{
+    var pool = dbconnection();
+  }catch(err){
+    console.log('Connect to database failed!')
+    callback(null);
+  }
+
+  if(req.params.value && req.params.user)
+  {
+    pool.connect().then(client => {
+      client.query(
+        "INSERT INTO MON_ENTRIES (value, crby, note) VALUES (" + req.params.value + ", '" + req.params.user + "', '" + req.params.note + "');"
+      ).then(res => {
+          const rowCreated = {
+          "rowCreated" : true,
+         };
+        result.send(rowCreated);			
+      })
+      .catch(e => {
+        console.error('query error', e.message, e.stack)
+        pool.end();
+      })
+      })
+  } else{
+    const wrongInput = {
+      "wrongInput" : true,
+     };
+    result.send(wrongInput);
+  }
 });
 
 app.get('/api/new/:user/:value', function(req, result, callback){
